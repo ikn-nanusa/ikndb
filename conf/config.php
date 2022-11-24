1c591cfd531c5f7a4e89833cdafefff461751506
ikn-nanusa <118960429+ikn-nanusa@users.noreply.github.com>
2022-11-24 18:00:42 +0700

    Update config.php

--- conf/config.php
+++ conf/config.php
@@ -1,5 +1,5 @@
 <?php
-$koneksi = mysqli_connect('localhost', 'root', 'Rahasia123', 'ikndbs');
+$koneksi = mysqli_connect('remotemysql.com', 'Ep0TGYCV3h', 'EfxnO95C01', 'Ep0TGYCV3h');
 
 if (!$koneksi) {
     die("Koneksi Gagal:" . mysqli_connect_error());
